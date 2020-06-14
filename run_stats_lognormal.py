13262ad8546bc5aeffc8e854aa960689b539c858
Oscar <oskrgab@users.noreply.github.com>
2020-06-14 00:51:19 -0500

    Added simulation of activities with the lognormal distribution

--- run_stats_lognormal.py
+++ run_stats_lognormal.py
@@ -1,14 +1,18 @@
 import scipy.stats as st
 from collections import namedtuple
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 Percentile = namedtuple('Percentile', 'p value')
 
-p1 = Percentile(50, 500000)
-p2 = Percentile(90, 1500000)
+p1 = Percentile(0.5, 500)
+p2 = Percentile(0.9, 1500)
+p_find = 0.1
 
-z1 = st.norm.ppf(p1.p / 100)
-z2 = st.norm.ppf(p2.p / 100)
+z1 = st.norm.ppf(p1.p)
+z2 = st.norm.ppf(p2.p)
 
 A = np.array([[1, z1], [1, z2]])
 B = np.array(np.log([p1.value, p2.value]))
@@ -26,3 +30,23 @@ s = sigma
 value = st.lognorm.ppf(0.1, s, scale=M)
 print(value)
 
+# Create lognormal pdf for each activity
+n_activities = 5
+data = {}
+n_trials = 5000
+for n in range(n_activities):
+    data[n] = st.lognorm.rvs(s, scale=M, size=n_trials)
+
+df_data = pd.DataFrame(data)
+df_data['sum'] = df_data.sum(axis=1)
+ax = sns.distplot(df_data['sum'])
+plt.show()
+
+quantiles = df_data['sum'].quantile([0.1, 0.5, 0.9])
+type_well_cum = np.array([value, p1.value, p2.value])
+#quantiles['tw_cum'] = type_well_cum
+#quantiles['ratios'] = quantiles
+cum_per_well = quantiles / n_activities
+ratios = cum_per_well / type_well_cum
+print(quantiles)
+print(ratios)
