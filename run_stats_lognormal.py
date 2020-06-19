5e3626f09c313b6eee713a7dd36055b302652223
Oscar <oskrgab@users.noreply.github.com>
2020-06-19 16:14:58 -0500

    Added distribution plots

--- run_stats_lognormal.py
+++ run_stats_lognormal.py
@@ -27,9 +27,29 @@ print(f'sigma: {sigma}')
 
 M = np.exp(mu)
 s = sigma
-value = st.lognorm.ppf(0.1, s, scale=M)
+value = st.lognorm.ppf(p_find, s, scale=M)
 print(value)
 
+# Plot the fitted lognormal distribution
+fig_0, ax_0 = plt.subplots(1, 1)
+
+min = st.lognorm.ppf(0.01, s, scale=M)
+max = st.lognorm.ppf(0.99, s, scale=M)
+n_points = 100
+x = np.linspace(min, max, n_points)
+
+ax_0.plot(x, st.lognorm.cdf(x, s, scale=M), 'b-', lw=1, alpha=0.6, label='lognorm cdf')
+x_scatter = [p1.value, p2.value, value]
+y_scatter = [p1.p, p2.p, p_find]
+colors = ['b', 'b', 'r']
+ax_0.scatter(x_scatter, y_scatter, c=colors)
+ax_0.set_ylabel('CDF')
+ax_0.set_title(f'Log Normal Distribution - Find {p_find} quantile given {(p1.p, p2.p)}')
+# Plot the pdf
+ax_1 = ax_0.twinx()
+ax_1.plot(x, st.lognorm.pdf(x, s, scale=M), 'b-', lw=1, alpha=0.6, label='lognorm pdf')
+ax_1.set_ylabel('PDF')
+plt.show()
 # Create lognormal pdf for each activity
 n_activities = 5
 data = {}
@@ -39,7 +59,9 @@ for n in range(n_activities):
 
 df_data = pd.DataFrame(data)
 df_data['sum'] = df_data.sum(axis=1)
-ax = sns.distplot(df_data['sum'])
+
+fig_1, ax_2 = plt.subplots(1, 1)
+sns.distplot(df_data['sum'], ax=ax_2)
 plt.show()
 
 quantiles = df_data['sum'].quantile([0.1, 0.5, 0.9])
